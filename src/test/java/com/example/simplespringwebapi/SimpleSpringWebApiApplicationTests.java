318260b09e3390db783daf22be1aac2cdf472e66
Ilyenkov <a.ilyenkov@mail.ru>
2021-05-25 16:48:51 +0300

    Project init

---
+++ src/test/java/com/example/simplespringwebapi/SimpleSpringWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
