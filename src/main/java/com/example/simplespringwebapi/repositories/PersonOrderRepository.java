0bea3779f3e19b74b6875dc3d00e040810eb2a69
Ilyenkov <a.ilyenkov@mail.ru>
2021-05-25 21:10:02 +0300

    added Demo Data

--- src/main/java/com/example/simplespringwebapi/repositories/PersonOrderRepository.java
+++ src/main/java/com/example/simplespringwebapi/repositories/PersonOrderRepository.java
@@ -3,5 +3,8 @@ package com.example.simplespringwebapi.repositories;
 import com.example.simplespringwebapi.entities.PersonOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonOrderRepository extends JpaRepository<PersonOrder, Long> {
+    List<PersonOrder> findByPersonId(long id);
 }
